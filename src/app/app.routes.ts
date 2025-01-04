badf92e546dcdc4d15f5d4f8796c395f0217856b
khalil-feki <khalilfeki8@gmail.com>
2025-01-04 16:43:22 +0100

    add Acceuil and Notfound components with styles and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,14 +5,16 @@ import { RegisterComponent } from './auth/register/register.component';
 import { LoginComponent } from './auth/login/login.component';
 import { EnseignantComponent } from './components/enseignant/enseignant.component';
 import { EtudiantComponent } from './components/etudiant/etudiant.component';
+import { AcceuilComponent } from './components/acceuil/acceuil.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/auth/login', pathMatch: 'full' }, // Redirect to login by default
+  { path: '', component: AcceuilComponent }, // Redirect to login by default
   { path: 'ticket', component: TicketComponent }, // Ticket route
   { path: 'support', component: SupportComponent }, // Support route
   { path: 'auth/register', component: RegisterComponent }, // Register route
   { path: 'auth/login', component: LoginComponent }, // Login route
   { path: 'enseignant', component: EnseignantComponent }, // Enseignant dashboard
   { path: 'etudiant', component: EtudiantComponent }, // Étudiant dashboard
-  { path: '**', redirectTo: '/auth/login' }, // Wildcard route (fallback to login)
+  { path: '**',component: NotfoundComponent }, // Wildcard route (fallback to login)
 ];
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
-export class NavbarComponent {
-
-}
+export class NavbarComponent {}
