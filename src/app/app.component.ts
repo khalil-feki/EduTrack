f769ba1122392e056a471978272b505321601dd2
khalil-feki <khalilfeki8@gmail.com>
2025-01-04 16:19:50 +0100

    add initial components and styles for authentication and navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './auth/login/login.component';
+import { EtudiantComponent } from './components/etudiant/etudiant.component';
+import { EnseignantComponent } from './components/enseignant/enseignant.component';
+import { SupportComponent } from './components/support/support.component';
+import { TicketComponent } from './components/ticket/ticket.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    // LoginComponent,
+    // EtudiantComponent,
+    // EnseignantComponent,
+    // SupportComponent,
+    // TicketComponent,
+    // RegisterComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Front';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { TicketComponent } from './components/ticket/ticket.component';
+import { SupportComponent } from './components/support/support.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { LoginComponent } from './auth/login/login.component';
+import { EnseignantComponent } from './components/enseignant/enseignant.component';
+import { EtudiantComponent } from './components/etudiant/etudiant.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/auth/login', pathMatch: 'full' }, // Redirect to login by default
+  { path: 'ticket', component: TicketComponent }, // Ticket route
+  { path: 'support', component: SupportComponent }, // Support route
+  { path: 'auth/register', component: RegisterComponent }, // Register route
+  { path: 'auth/login', component: LoginComponent }, // Login route
+  { path: 'enseignant', component: EnseignantComponent }, // Enseignant dashboard
+  { path: 'etudiant', component: EtudiantComponent }, // Étudiant dashboard
+  { path: '**', redirectTo: '/auth/login' }, // Wildcard route (fallback to login)
+];
